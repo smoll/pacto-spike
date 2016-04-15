f5d8ef380c86e7ab873fe7c51208b8155aa153c1
Shujon Mollah <mollah@gmail.com>
2016-04-15 13:09:41 -0400

    Get 'PACTO_MODE=generate rake' to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+require 'pacto'
+require 'pacto/rspec'
+
+Pacto.configure do |config|
+  config.contracts_path = 'contracts'
+end
 
 RSpec.configure do |c|
 end
+
+pacto_mode = ENV['PACTO_MODE']
+require "pacto_modes/#{pacto_mode}" if pacto_mode
+
+WebMock.allow_net_connect!
