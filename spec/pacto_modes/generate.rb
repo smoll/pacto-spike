be093f3597a174a54a6740f99a8e3ca917abfded
Shujon Mollah <mollah@gmail.com>
2016-04-15 13:47:36 -0400

    Get validate build to work

--- spec/pacto_modes/generate.rb
+++ spec/pacto_modes/generate.rb
@@ -1,1 +1,4 @@
 Pacto.generate!
+
+# The generate build must make a live connection in order to generate the json contract
+WebMock.allow_net_connect!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,3 @@ end
 
 pacto_mode = ENV['PACTO_MODE']
 require "pacto_modes/#{pacto_mode}" if pacto_mode
-
-WebMock.allow_net_connect!
