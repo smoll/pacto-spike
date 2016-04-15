1098cf48a1f27e430f6938cde46f1ed45a0b2db4
Shujon Mollah <mollah@gmail.com>
2016-04-15 14:31:01 -0400

    Expect Pacto to have validated, in validate build

---
+++ README.md
@@ -0,0 +1,1 @@
+# pacto-spike [![Circle CI](https://circleci.com/gh/smoll/pacto-spike.svg?style=svg)](https://circleci.com/gh/smoll/pacto-spike)
--- spec/external_request_spec.rb
+++ spec/external_request_spec.rb
@@ -8,6 +8,13 @@ describe 'External request', :vcr => true do
     response = do_request(uri)
 
     expect(response).to be_an_instance_of(Net::HTTPOK)
+
+    if ENV["PACTO_MODE"] == "validate"
+      expect(Pacto).to have_validated(:get, 'https://api.github.com/repos/thoughtbot/factory_girl/contributors')
+      # Pacto API changed? getting: expected Pacto to respond to `has_failed_validations?`
+      # expect(Pacto).to_not have_failed_validations
+      expect(Pacto).to_not have_unmatched_requests
+    end
   end
 
   def do_request(uri)
